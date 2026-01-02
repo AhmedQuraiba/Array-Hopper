0bb6c5363cd5524bb40eda02973c5d012d10ff33
Ahmed <talosmedal@gmail.com>
2026-01-02 17:01:37 +0300

    Added the ability to utils file to take user input and perform verification on it.

--- utils.py
+++ utils.py
@@ -18,3 +18,24 @@ def show_screen(matrix):
     print(f"|{width*" "}|")
     #upper-border outside the matrix
     print(f" ‾{(width-2)*" "}‾")
+
+#options is an array of strings repersenting user choices
+#message is the message represented to the user before selecting an item
+def get_input(message, options):
+    option_range = len(options)
+
+    print(f"{message}")
+    while True:
+        for option in range (option_range):
+            print(f"{option+1}-{options[option]}")
+        
+        try:
+            user_choice = int(input(">"))
+            
+            if user_choice in range(1,option_range+1):
+                print(user_choice,options[user_choice-1])
+                break
+            else:
+                print(f"\nPlease enter a number between 1 and {option_range}")
+        except:
+            print("\nPlease enter an integer.") #To catch decimals and letters
